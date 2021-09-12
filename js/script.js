caa2db94e27a7b582f2381ab85c332f05e9622b6
LearnGitHub <thanhnd311020@gmail.com>
2021-09-12 15:28:53 +0700

    update project

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ window.onscroll = () =>{
         header.classList.remove('active');
     }
 }
+
+
+let countDate = new Date('2021-10-25 00:00:00').getTime();
+console.log(countDate)
+function CountDown(){
+
+    let now = new Date().getTime();
+    // console.log(now);
+     gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+  
+}
+
+setInterval(function(){
+    CountDown();
+},1000);
